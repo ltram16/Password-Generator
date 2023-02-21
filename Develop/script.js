98825249b80f80961eefaf3d0c64ccd1992acd74
ltram16 <ltram16@gmail.com>
2023-02-20 20:45:27 -0800

    added alphabets, numbers, and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Alphabet, Numbers, Special Characters
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowercase = ["a", "b", "c", "d", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialCharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", ",", ";", ":", "{", "}", "[", "]", "/", "?", "-", "_", "."];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
